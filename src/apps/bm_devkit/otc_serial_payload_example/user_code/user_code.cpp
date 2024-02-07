af59c263e853829b945ed97170320fbaf5a333e3
Aadu <Aadu>
2024-02-06 22:30:54 -0800

    Added some logic to parse Aanderaa data

--- src/apps/bm_devkit/otc_serial_payload_example/user_code/user_code.cpp
+++ src/apps/bm_devkit/otc_serial_payload_example/user_code/user_code.cpp
@@ -9,6 +9,7 @@
 #include "lwip/inet.h"
 #include "payload_uart.h"
 #include "sensors.h"
+#include "OrderedSeparatorLineParser.h"
 #include "stm32_rtc.h"
 #include "task_priorities.h"
 #include "uptime.h"
@@ -23,6 +24,12 @@ static int32_t ledLinePulse = -1;
 
 // A buffer for our data from the payload uart
 char payload_buffer[2048];
+char disable_text[] = "set enable text(no)\r";
+char get_sample[] = "do sample\r";
+
+// Eg: 4330 740 269.539 100.322 22.826
+ValueType valueTypes[] = {TYPE_UINT64, TYPE_UINT64, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE};
+OrderedSeparatorLineParser parser(" ", 256, valueTypes, 5);
 
 void setup(void) {
   /* USER ONE-TIME SETUP CODE GOES HERE */
@@ -34,7 +41,7 @@ void setup(void) {
   PLUART::setTerminationCharacter('\n');
   // Turn on the UART.
   PLUART::enable();
-
+  // Initialize Debug PLUART CLI 
   debugPlUartCliInit();
   // Enable the input to the Vout power supply.
   bristlefin.enableVbus();
@@ -42,14 +49,30 @@ void setup(void) {
   vTaskDelay(pdMS_TO_TICKS(5));
   // enable Vout, 12V by default.
   bristlefin.enableVout();
+
+  parser.init();
+  userConfigurationPartition->getConfig("plUartBaudRate", strlen("plUartBaudRate"));
+
+  // Disable text
+  PLUART::write(&disable_text[0], sizeof(disable_text));
 }
 
 void loop(void) {
   /* USER LOOP CODE GOES HERE */
+  PLUART::write(&get_sample[0], sizeof(get_sample));
 
   // Read a line if it is available
   if (PLUART::lineAvailable()) {
     uint16_t read_len = PLUART::readLine(payload_buffer, sizeof(payload_buffer));
+    
+    // Now when we get a line of text data, our LineParser turns it into numeric values.
+    if (parser.parseLine(payload_buffer, read_len)) {
+      printf("parsed values: %" PRIu64 " | %" PRIu64 " | %f | %f | %f\n", parser.getValue(0).data, parser.getValue(1).data,
+                                                                          parser.getValue(2).data, parser.getValue(3).data,
+                                                                          parser.getValue(4).data);
+    } else {
+      printf("Error parsing line!\n");
+    }
 
     // Get the RTC if available
     RTCTimeAndDate_t time_and_date = {};
@@ -58,13 +81,9 @@ void loop(void) {
     rtcPrint(rtcTimeBuffer, &time_and_date);
 
     // Print the payload data to a file, to the bm_printf console, and to the printf console.
-    bm_fprintf(0, "payload_data.log", "tick: %llu, rtc: %s, line: %.*s\n",
-               uptimeGetMs(), rtcTimeBuffer, read_len, payload_buffer);
-    bm_printf(0, "[payload] | tick: %llu, rtc: %s, line: %.*s", uptimeGetMs(),
-              rtcTimeBuffer, read_len, payload_buffer);
-    printf("[payload] | tick: %llu, rtc: %s, line: %.*s\n", uptimeGetMs(),
-           rtcTimeBuffer, read_len, payload_buffer);
-
-    ledLinePulse = uptimeGetMs(); // trigger a pulse on LED2
+    bm_fprintf(0, "payload_data.log", "tick: %llu, rtc: %s, line: %.*s\n", uptimeGetMs(), rtcTimeBuffer, read_len, payload_buffer);
+    //bm_printf(0, "[payload] | tick: %llu, rtc: %s, line: %.*s", uptimeGetMs(), rtcTimeBuffer, read_len, payload_buffer);
+    printf("[payload] | tick: %llu, rtc: %s, line: %.*s\n", uptimeGetMs(), rtcTimeBuffer, read_len, payload_buffer);
   }
+  vTaskDelay(pdMS_TO_TICKS(30000));
 }
